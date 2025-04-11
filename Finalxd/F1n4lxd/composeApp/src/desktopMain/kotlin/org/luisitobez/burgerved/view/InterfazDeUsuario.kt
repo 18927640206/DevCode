9bdba6d7d3018a670f6d12df95cd6bb1df5069ac
Luisitobez <luisitobez@gmail.com>
2025-04-10 23:40:15 -0600

    1.6.5

--- Finalxd/F1n4lxd/composeApp/src/desktopMain/kotlin/org/luisitobez/burgerved/view/InterfazDeUsuario.kt
+++ Finalxd/F1n4lxd/composeApp/src/desktopMain/kotlin/org/luisitobez/burgerved/view/InterfazDeUsuario.kt
@@ -72,6 +72,7 @@ class InterfazDeUsuario() : Screen {
                 .onKeyEvent { keyEvent ->
                     if (keyEvent.isCtrlPressed) {
                         botonAdmin = true// Alternar estado con Ctrl+A
+                        println("Hola")
                         true
                     } else {
                         botonAdmin = false// Alternar estado con Ctrl+A
@@ -254,7 +255,7 @@ class InterfazDeUsuario() : Screen {
                     carritoController.eliminarPedido(pedido)
                     pedido = appController.pedidoController.agregarPedido()!!
                     productospedido = productoController.pedirPedidoProductos(pedido)
-                },  enabled = productospedido.isNotEmpty()
+                },  //enabled = productospedido.isNotEmpty()
                     ) {
                     Text("Cancelar", fontSize = 24.sp)
                 }
